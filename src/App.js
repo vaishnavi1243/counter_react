d806579e19ed2350f6420fe8fa4a7d773fa5d9e8
vaishnavi <vaishnavi@divami.com>
2023-02-22 10:07:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { useState } from "react";
+export default function App() {
+  let [count, setCount] = useState(0);
+  const style = {
+    height: "50px",
+    width: "50px",
+    border: "2px solid black",
+  };
+  return (
+    <>
+      <center>
+        <div style={style}>{count}</div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount((count = 0))}>reset</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+      </center>
+    </>
+  );
+}
